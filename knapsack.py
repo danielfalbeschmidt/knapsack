59aeda40de6fdac4b55f99e18c9f2858cb2a393b
Daniel Schmidt <daniel.f.schmidt@outlook.com>
2023-09-15 10:39:28 +0300

    weights added, permutation scores

--- knapsack.py
+++ knapsack.py
@@ -1,36 +1,80 @@
-import math, random
+import random
 
 class K:
 
-   items = [3, 4, 5]
+   max_item_size = 10000000000 # big values for big randomness
+   max_item_weight = 10000000000
+   # item_count = 3 # use wisely, permutation count becomes the factorial of item_count
+   item_count = 3 # !!! for now, only item_count of 3 is supported
+   weights = [1, 2, 3] # item "goodness" values, i.e. greater is better (pure gold!)
+   sizes = [3, 4, 5] # item sizes in relation to sack_vol
    sack_vol = 11 # must be less than the sum of items
+   item_orders = [] # values for indexing K.sizes
+   perm_inds = [] # indices of permutations used in item_orders
+
+   def init(item_count):
+      K.weights = []
+      K.sizes = []
+      K.perm_inds = []
+
+      for _ in range(item_count):
+         K.sizes.append(random.randint(1, K.max_item_size))
+         K.weights.append(random.randint(1, K.max_item_weight))
+
+      K.sack_vol = random.randint(min(K.sizes), sum(K.sizes))
+      K.sizes = sorted(K.sizes)
+      K.item_orders = K.getItemOrders()
+
+
+   def printSpecs():
+      print(f'Weights: {K.weights}')
+      print(f'Sizes: {K.sizes}')
+      print(f'Sack Volume: {K.sack_vol}')
+      print(f'Permutations used, indices: {K.perm_inds}')
+
 
    def getItemOrders(): # filter overlapping permutations
       item_orders = [] # unique list of item index lists that fit the sack
 
       permutations = K.getPermutations()
-      for p in permutations:
-         item_order = [] # one index list, referenced items fit the sack
-         item_sum = 0 # cumulative sum, compared to sack_vol
-         
-         for index in p:
-            items = K.items[index]
-            if item_sum + items > K.sack_vol: break
-            item_order.append(index)
-            item_sum += items
 
-         order_exist = False
-         for items in item_orders:
-            if set(items) == set(item_order):
-               order_exist = True
-               break
-         if not order_exist and item_order:
-            item_orders.append(sorted(item_order))
+      item_orders.append(K.getLimitedItems(permutations[0]))
+      K.perm_inds.append(0)
 
-         if not item_orders and item_order:
-            item_orders.append(sorted(item_order))
+      i = 0
+      for p in permutations[1:]:
+         i += 1
+
+         # get as many items of this p that can fit the sack
+         new_item_order = K.getLimitedItems(p)
+
+         # eliminate duplicate sets of items
+         order_exist = K.orderExist(item_orders, new_item_order)
+         if not order_exist and new_item_order:
+            item_orders.append(new_item_order)
+            K.perm_inds.append(i)
 
       return item_orders
+   
+
+   def getLimitedItems(item_permutation):
+      item_order = [] # one index list, referenced items fit the sack
+      item_sum = 0 # cumulative sum, compared to sack_vol
+
+      for index in item_permutation:
+         items = K.sizes[index]
+         if item_sum + items > K.sack_vol: break
+         item_order.append(index)
+         item_sum += items
+
+      return sorted(item_order)
+   
+
+   def orderExist(item_orders, compare_order):
+      for items in item_orders:
+         if set(items) == set(compare_order): return True
+      return False
+
 
    def getPermutations():
       # heap's algorithm would be great
@@ -44,15 +88,23 @@ class K:
          , [2, 1, 0]
       ]
    
+
    def fillSack(item_order):
-      sack_state = 0
+      product_sum = 0 # sizes * weights
+      sack_state = 0 # occupied volume
 
-      # fill the sack, spare space should be guaranteed
+      # fill the sack, all items are expected to fit (calculated elsewhere)
       for index in item_order:
-         sack_state += K.items[index]
+         item_size = K.sizes[index]
+         sack_state += item_size
+         product_sum += item_size * K.weights[index]
 
-      # return leftover space in sack
-      return K.sack_vol - sack_state
+      spare_space = K.sack_vol - sack_state
+
+      # goal: minimize spare_space and maximize product_sum
+      return product_sum - spare_space
+   
+
    
 
 
--- main.py
+++ main.py
@@ -1,7 +1,38 @@
+import matplotlib.pyplot as plt
 from knapsack import K
 
-for p in K.getItemOrders():
-    print(K.fillSack(p))
+r = [0, 0, 0, 0, 0, 0]
+iterations = 1000
 
+for _ in range(iterations):
+    K.init(3)
+    # K.printSpecs()
+
+    for i in range(len(K.item_orders)):
+        perm_index = K.perm_inds[i]
+        item_order = K.item_orders[i]
+        
+        sack_state = K.fillSack(item_order)
+        
+        r[perm_index] += sack_state
+
+
+m = max(r)
+
+for i in range(len(r)):
+    r[i] = round( r[i] / m, 2 )
+
+print(r)
+
+plt.bar([0,1,2,3,4,5], r)
+ 
+plt.xlabel('Permutation no.')
+plt.ylabel('Performance (perm. points = size * weight - waste_space)\n')
+plt.title(f'Three item knapsack problem, trial count {iterations}.\n'\
+            'Items are ordered by size (asc). Randomized values:\n'\
+            'sizes, weigths, sack_vol (with limitations). For\n'\
+            'each indexing permutation, items are added from left\n'\
+            'to right until no room for next.')
+plt.show()
 
 
