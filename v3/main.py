4126874f602b093fbdc62dd318a21cf712d4ed04
Daniel Schmidt <daniel.f.schmidt@outlook.com>
2023-10-20 08:59:18 +0300

    data file creation setup

--- v3/main.py
+++ v3/main.py
@@ -1,8 +1,7 @@
-import copy
+import copy, datetime
 from settings import *
 from reserve import *
 from sack import *
-from nn import *
 
 def getFilledSack(reserve):
     sack = Sack()
@@ -12,21 +11,46 @@ def getFilledSack(reserve):
         if not sack.add(item): return sack
 
 
-nn.initWeights()
+filename = datetime.datetime.now()
+file = open(f'./v3/data/{filename}', 'w')
+file.write(S.toString())
+file.write('DATA START\n')
+file.close()
 
-reserve = Reserve()
 
-reserve_distr = (
-    reserve.getVolumeDistribution() +
-    reserve.getWeightDistribution()
-    )
+for _ in range(S.total_iterations):
+    sack_distrs = []
+    sack_values = []
+    reserve = Reserve()
 
-for __ in range(S.iterations_per_reserve):
-    res_copy = copy.deepcopy(reserve)
-    filled_sack = getFilledSack(res_copy)
+    for __ in range(S.iterations_per_reserve):
+        res_copy = copy.deepcopy(reserve)
+        filled_sack = getFilledSack(res_copy)
 
-    sack_distr = filled_sack.getVolumeDistribution()
-    sack_value = filled_sack.getTotalValue()
+        if filled_sack.getTotalVolume() > res_copy.getTotalVolume():
+            print('All reserve items fits in the sack, discarded...')
+            continue
 
-    nn.learn(reserve_distr, sack_distr, sack_value)
-    nn.printOutputWeights()
+        sack_distrs.append(filled_sack.getVolumeDistribution())
+        sack_values.append(filled_sack.getTotalValue())
+
+    best_sack_index = sack_values.index(max(sack_values))
+    best_sack_distr = sack_distrs[best_sack_index]
+
+
+    data_str = str( (
+        (
+            reserve.getVolumeDistribution(),
+            reserve.getWeightDistribution()
+        ),
+        best_sack_distr
+        ) )
+
+    file = open(f'./v3/data/{filename}', 'a')
+    file.write(f'{data_str}\n')
+    file.close()
+
+
+file = open(f'./v3/data/{filename}', 'a')
+file.write('DATA END\n')
+file.close()
--- v3/settings.py
+++ v3/settings.py
@@ -1,9 +1,21 @@
 class S:
-    volume_category_count = 6
-    weight_category_count = 6
-    reserve_item_count = 100
-    sack_volume = 20
-    iterations_per_reserve = 100
-    learning_rate = 1 # bigger for faster learning
-    bias = 0
-    hidden_layer_count = 6
+    volume_category_count = 6 # arbitrary
+    weight_category_count = 6 # arbitrary
+
+    reserve_item_count = 60 # affects data creation computation time
+    sack_volume = reserve_item_count / 5 # heuristical, too small value -> all items fit (no point regarding KP)
+
+    iterations_per_reserve = 1000 # arbitrary
+    total_iterations = 100000 # arbitrary
+
+    data_description = '(([ reserve_volume_distr ], [ reserve_weight_distr ]), [ winner_sack_volume_distr ])'
+
+    def toString():
+        return \
+            f'volume_category_count: {S.volume_category_count}\n'\
+            f'weight_category_count: {S.weight_category_count}\n'\
+            f'reserve_item_count: {S.reserve_item_count}\n'\
+            f'sack_volume: {S.sack_volume}\n'\
+            f'iterations_per_reserve: {S.iterations_per_reserve}\n'\
+            f'data description: {S.data_description}\n'
+            
